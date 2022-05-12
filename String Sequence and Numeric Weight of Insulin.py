88205b27da4346f65c5456e322465d0b13ecd52f
Sushanth Kumar <sushanth@rooman.com>
2022-05-12 10:07:28 +0530

    Initial commit

---
+++ String Sequence and Numeric Weight of Insulin.py
@@ -0,0 +1,7 @@
+# Python version: 3.10.4
+# Encoding - UTF-8
+
+#  This program stores the protein sequence of human preproinsulin in a string
+#  variable and the weight of preproinsulin in int and float variables
+
+print("\n Hello \n")
