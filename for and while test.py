a085e5117455d1ba51cb45ef3aa37f802ff8a874
Sushanth Kumar <sushanth@rooman.com>
2022-05-13 11:58:24 +0530

    commit on Day5

---
+++ for and while test.py
@@ -0,0 +1,24 @@
+"""
+pH=0
+while(pH<=14):
+    print(pH)
+    # some more calculations
+    pH+=1
+
+pH2=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+for pHlevel in pH2:
+    print(pH2[pHlevel])
+    # some more calculations
+"""
+
+a=3
+b=2
+c=1
+def demo(a,b,c):
+    y=(a+b+c)
+    print(y)
+    return y
+
+d=demo(a,b,c)
+print(d)
+
